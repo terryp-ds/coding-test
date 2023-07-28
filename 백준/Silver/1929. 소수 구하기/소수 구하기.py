62e33c1365b9cd6ccac9436ccd084db10097bbed
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-07-28 15:10:21 +0900

    [Silver III] Title: 소수 구하기, Time: 3148 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,16 @@
+def is_prime(n):
+    if n == 1:
+        return False
+    
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+        
+    return True
+
+
+m,n = list(map(int, input().split()))
+
+for i in range(m,n+1):
+    if is_prime(i):
+        print(i)
