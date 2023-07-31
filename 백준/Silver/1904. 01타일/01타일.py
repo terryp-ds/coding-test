78989cc6a7bc486fbd654dc31c9e1f15084ce6f2
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-07-31 13:02:10 +0900

    [Silver III] Title: 01타일, Time: 208 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,10 +1,7 @@
 n = int(input())
+a = b = 1
 
-dic = {1:1, 2:2}
+for i in range(n-1):
+    a,b = b, (a+b) % 15746
 
-for i in range(1, n+1):
-    if i not in dic:
-        dic[i] = (dic[i-1]+dic[i-2]) % 15746
-
-
-print(dic[n])
+print(b)
