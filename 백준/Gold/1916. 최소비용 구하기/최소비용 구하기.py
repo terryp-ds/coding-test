05d5e1a1d73e8ba17e2cc0e453f6ff099b5564b9
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-08-06 14:53:35 +0900

    [Gold V] Title: 최소비용 구하기, Time: 260 ms, Memory: 57508 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -3,30 +3,28 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-m = int(input())
-edges = [list(map(int, input().split())) for _ in range(m)]
+graph = [[] for _ in range(n+1)]
+
+for _ in range(int(input())):
+    u,v,w = map(int, input().split())
+    graph[u].append((w,v))
+
+visited = [float('inf')]*(n+1)
 start, end = map(int, input().split())
-graph = [[] for _ in range(n)]
-dist = [10**10]*n
-dist[start-1] = 0
 
-for v1,v2,w in edges:
-    graph[v1-1].append((v2-1, w))
-
-heap = [(0, start-1)]
+heap = [(0,start)]
 
 while heap:
     cost, node = heapq.heappop(heap)
 
-    if node == end-1:
+    if node == end:
         break
 
-    for v2,w in graph[node]:
-        alt = cost + w
+    for cost2, node2 in graph[node]:
+        c = cost + cost2
 
-        if alt < dist[v2]:
-            heapq.heappush(heap, (alt, v2))
-            dist[v2] = alt
+        if c < visited[node2]:
+            visited[node2] = c
+            heapq.heappush(heap, (c, node2))
 
-
-print(dist[end-1])
+print(visited[end])
