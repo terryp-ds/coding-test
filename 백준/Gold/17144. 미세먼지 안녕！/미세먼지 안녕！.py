c7511d3388da02daaf82062f18d447e7561afce4
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-08-08 12:44:43 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 444 ms, Memory: 116648 KB -BaekjoonHub

---
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -0,0 +1,65 @@
+import sys
+input = sys.stdin.readline
+
+r,c,t = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(r)]
+dy = [1,-1,0,0]
+dx = [0,0,1,-1]
+f = 0
+
+for y in range(r):
+    if f:
+        break
+    
+    for x in range(c):
+        if arr[y][x] == -1:
+            loc = (y,x)
+            f = 1
+            break
+
+for _ in range(t):
+    arr2 = [[0]*c for _ in range(r)]
+
+    for y in range(r):
+        for x in range(c):
+            if arr[y][x] > 0:
+                cnt = 0
+                
+                for i in range(4):
+                    ny = y + dy[i]
+                    nx = x + dx[i]
+                    
+                    if 0 <= ny < r and 0 <= nx < c:
+                        if arr[ny][nx] != -1:
+                            arr2[ny][nx] += arr[y][x]//5
+                            cnt += 1
+            
+                arr2[y][x] += arr[y][x] - ((arr[y][x]//5)*cnt)
+
+            elif arr[y][x] == -1:
+                arr2[y][x] = -1
+    
+    y,x = loc
+    
+    for i in range(y-1, 0, -1):
+        arr2[i][0] = arr2[i-1][0]
+
+    for i in range(y+2, r-1):
+        arr2[i][0] = arr2[i+1][0]
+
+    for i in [0, -1]:
+        arr2[i] = arr2[i][1:]+[0]
+
+    for i in range(y):
+        arr2[i][-1] = arr2[i+1][-1]
+
+    for i in range(r-1, y+1, -1):
+        arr2[i][-1] = arr2[i-1][-1]
+
+    for i in range(y,y+2):
+        arr2[i] = [-1, 0] + arr2[i][1:-1]
+
+    arr = arr2
+    
+    
+print(sum(map(sum, arr))+2)
