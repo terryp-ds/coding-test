d08602041b8954aad56f4eb3a5bbd2a107ab9e43
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-08-16 13:54:49 +0900

    [Silver IV] Title: solved.ac, Time: 144 ms, Memory: 38352 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -1,4 +1,5 @@
 import sys
+input = sys.stdin.readline
 
 n = int(input())
 
@@ -6,7 +7,7 @@ if n == 0:
     print(0)
 
 else:
-    arr = [int(sys.stdin.readline().strip()) for _ in range(n)]
+    arr = [int(input()) for _ in range(n)]
     cut = int(n*0.15+0.5)
 
     print(int(sum(sorted(arr)[cut:len(arr)-cut])/(len(arr)-2*cut)+0.5))
