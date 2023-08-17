3bd0fc35c9fb91b797107eed060e4fc253c4a8c5
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-08-17 14:41:41 +0900

    [Silver III] Title: 통계학, Time: 392 ms, Memory: 54156 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -4,12 +4,9 @@ input = sys.stdin.readline
 
 n=int(input())
 s=sorted(int(input()) for i in range(n))
+c=Counter(s).most_common(2)*2
 
 print(int(round(sum(s)/n, 0)))
 print(s[n//2])
-    
-c=sorted(Counter(s).items(),key=lambda x:(x[1], x[0]))
-c=[i for i in c if i[1]==c[-1][1]]
-
-print(c[0][0] if len(c) == 1 else c[1][0])    
+print(c[1][0] if c[0][1] == c[1][1] else c[0][0])    
 print(s[-1]-s[0])
