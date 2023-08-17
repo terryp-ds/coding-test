a28e2b2f111c71cee219f530184d0091c9500acf
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-08-17 12:19:36 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,29 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-class Stack:
-    def __init__(self):
-        self.data=[]
-    def push(self, x):
-        self.data.append(x)
-    def pop(self):
-        if self.data:
-            print(self.data.pop())
-        else:
-            print(-1)
-    def size(self):
-        print(len(self.data))
-    def empty(self):
-        print(int(len(self.data)==0))
-    def top(self):
-        print(self.data[-1] if self.data else -1)  
+s = []
 
-n=int(input())
-c=[input().rstrip().split() for i in range(n)]
-s=Stack()
+for _ in range(int(input())):
+    
+    c = input().rstrip().split()
 
-for i in c:
-    if len(i) == 2:
-        eval(f's.push({i[1]})')
+    if c[0] == 'push':
+        s.append(c[1])
+
+    elif c[0] == 'pop':
+        print(s.pop() if s else -1)
+
+    elif c[0] == 'size':
+        print(len(s))
+
+    elif c[0] == 'empty':
+        print(int(len(s)==0))
+
     else:
-        eval(f's.{i[0]}()')
+        print(s[-1] if s else -1)
