98e3ef3d2e08a9f119b37f67c0ce05216c7718d7
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-08-22 14:02:12 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,19 +1,11 @@
-from collections import deque
-
 a,b = map(int, input().split())
-
-visited = {}
-
-queue = deque([a])
-visited[a] = 1
-
-while queue:
-    node = queue.popleft()
-
-    for next_node in [node*2, node*10+1]:
-            
-            if next_node <= b and next_node not in visited:
-                queue.append(next_node)
-                visited[next_node] = visited[node]+1
-
-print(visited[b] if b in visited else -1)
+c = 1
+while b > a:
+    if b%10 == 1:
+        b//=10
+    elif b%2:
+        break
+    else:
+        b//=2
+    c+=1
+print(c if a==b else -1)
