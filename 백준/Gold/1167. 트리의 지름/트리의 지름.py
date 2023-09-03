ef2c7b7ec38e2649f39bf9208d784e07068b4307
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-09-03 21:26:49 +0900

    [Gold II] Title: 트리의 지름, Time: 676 ms, Memory: 78360 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+g=[[] for _ in range(n+1)]
+
+for _ in range(n):
+    a=[*map(int, input().split())]
+
+    for j in range(len(a)//2-1):
+        g[a[j*2+1]].append((a[j*2+2],a[0]))
+
+def dfs(s):
+    v=[0]*(n+1)
+    q=[(0,s)]
+    v[s]=1
+    while q:
+        w,e=q.pop()
+        for w2,e2 in g[e]:
+            if not v[e2]:
+                q.append((w+w2,e2))
+                v[e2]+=w+w2
+
+    return (max(v),v.index(max(v)))
+
+print(dfs(dfs(1)[1])[0])
