e16ce85afb361a5fdb08e3ca9175901ab533fcfe
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-09-10 11:19:42 +0900

    [Silver IV] Title: 이상한 호텔의 송이, Time: 752 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/29702. 이상한 호텔의 송이/이상한 호텔의 송이.py
+++ 백준/Silver/29702. 이상한 호텔의 송이/이상한 호텔의 송이.py
@@ -1,9 +1,8 @@
 import sys
 input = sys.stdin.readline
-
 for _ in range(int(input())):
-    n = int(input())
-    while n > 0:
-        d = n.bit_length()
+    n=int(input())
+    while n>0:
+        d=n.bit_length()
         print(str(d)+str(n-(1<<(d-1))+1).zfill(18))
         n>>=1
