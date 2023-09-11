beea4fab2c30492ae11869c8d3bf5b7464e42bb3
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-09-11 13:39:42 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 516 ms, Memory: 60772 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -1,28 +1,25 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-v,e,r = map(int, input().split())
+n,m,r=map(int, input().split())
+g=[[] for _ in range(n+1)]
 
-graph = [[] for _ in range(v)]
+for _ in range(m):
+    s,e=map(int, input().split())
+    g[s].append(e)
+    g[e].append(s)
 
-for _ in range(e):
-    a,b=map(int, input().split())
-    graph[a-1].append(b-1)
-    graph[b-1].append(a-1)
-    
-visited = [0]*v
-stack = [r-1]
-count = 1
+v=[0]*(n+1)
+s=[r]
+o=1
 
-while stack:
-    vertex = stack.pop()
-    
-    if not visited[vertex]:
-        visited[vertex] += count
-        count += 1
-        for neighbor in sorted(graph[vertex], reverse=True):
-            if not visited[neighbor]:
-                stack.append(neighbor)
-                
+while s:
+    c=s.pop()
+    if not v[c]:
+        v[c]=o
+        o+=1
+        for x in sorted(g[c], reverse=True):
+            if not v[x]:
+                s.append(x)
 
-print(*visited, sep='\n')
+print(*v[1:], sep='\n')
