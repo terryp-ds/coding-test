c7a837c417062f607f88fc10aae103c669e9386f
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-09-13 00:23:33 +0900

    Update 태권왕.py

--- 백준/Silver/14562. 태권왕/태권왕.py
+++ 백준/Silver/14562. 태권왕/태권왕.py
@@ -4,14 +4,14 @@ input = sys.stdin.readline
 
 for _ in range(int(input())):
     s,t=map(int,input().split())
-    d={(s,t):0}
+    v={(s,t):0}
     q=deque([(s,t)])
     while q:
         s,t=q.popleft()
         if s==t:
             break
         for c in [(s*2,t+3),(s+1,t)]:
-            if c[0]<=c[1] and c not in d:
+            if c[0]<=c[1] and c not in v:
                 q.append(c)
-                d[c]=d[(s,t)]+1
-    print(d[(s,t)])
+                v[c]=d[(s,t)]+1
+    print(v[(s,t)])
