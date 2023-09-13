aaa0ee48bbba07e664cb64b3d0a322401728a685
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-09-13 16:22:58 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34276 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,41 +1,26 @@
 from collections import deque
 import sys
+input=sys.stdin.readline
 
-input = sys.stdin.readline
-n,m = map(int,input().split())
-maps = [list(map(int, input().strip())) for _ in range(n)]
-graph = [[] for _ in range(n*m)]
+n,m=map(int,input().split())
+a=[[*map(int,input().rstrip())] for _ in range(n)]
+v=[[0]*m for _ in range(n)]
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
+dy=[-1,1,0,0]
+dx=[0,0,-1,1]
 
-for y in range(n):
-    for x in range(m):
-        if maps[y][x]:
-            for i in range(4):
-                ny = y+dy[i]
-                nx = x+dx[i]
-                if 0 <= nx < m and 0 <= ny < n and maps[ny][nx]:
-                    graph[x+y*m].append(nx+ny*m)
+q=deque([(0,0)])
+v[0][0]=1
 
-visited = [0]*n*m
-route = []
-queue = deque([0])
-pred = [[] for _ in range(n*m)]
+while q:
+    y,x=q.popleft()
+    
+    for i in range(4):
+        ny=y+dy[i]
+        nx=x+dx[i]
 
-while queue:
-    cur = queue.popleft()
+        if 0<=ny<n and 0<=nx<m and a[ny][nx] and not v[ny][nx]:
+            q.append((ny,nx))
+            v[ny][nx]=v[y][x]+1
 
-    if cur == n*m-1:
-        print(len(pred[-1])+1)
-        break
-
-    if not visited[cur]:
-        visited[cur] = 1
-        route.append(cur)
-
-        for nex in graph[cur]:
-            if not visited[nex]:
-                queue.append(nex)
-                if not pred[nex]:
-                    pred[nex].extend(pred[cur]+[cur])
+print(v[-1][-1])
