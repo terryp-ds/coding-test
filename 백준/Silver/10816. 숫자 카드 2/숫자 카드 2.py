241cdde47ae4c8ac744708d89a686f6e3dc864a6
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-10-05 09:34:25 +0900

    Update 숫자 카드 2.py

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,9 +1,10 @@
 from collections import Counter
 import sys
-input()
-a=sorted([int(i) for i in sys.stdin.readline().strip().split()])
-input()
-b=[int(i) for i in sys.stdin.readline().strip().split()]
+input=sys.stdin.readline
 
+input()
+a=sorted([*map(int,input().split())])
+input()
+b=[*map(int,input().split())]
 c=Counter(a)
-print(' '.join([f'{c[i]}' if i in c else '0' for i in b]))
+print(*[c[i] for i in b])
