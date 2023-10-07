a93220caa9df39b1612dff4bebf106f4fbeaba77
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-10-07 13:39:00 +0900

    Update 먹을 것인가 먹힐 것인가.py

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,10 +1,5 @@
 from bisect import *
-import sys
-input=sys.stdin.readline
-
 for _ in range(int(input())):
     n,m=map(int,input().split())
     a,b=[sorted([*map(int,input().split())]) for _ in range(2)]
-    c=0
-    for i in a: c+=bisect_left(b,i)
-    print(c)
+    print(sum([bisect_left(b,i) for i in a]))
