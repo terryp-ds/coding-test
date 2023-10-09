755c2d776b03c9b8d891c8ed6c2fd440e0714b12
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-10-09 11:28:18 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 3304 ms, Memory: 67364 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -1,3 +1,7 @@
+d=10**9+7
 n=int(input())
 a=sorted([*map(int,input().split())])
-print((sum([a[i]*(2**i-1) for i in range(n)])-sum([a[i]*(2**(n-1-i)-1) for i in range(n)]))%(10**9+7))
+def s(i):
+    if i<2: return 2**i
+    else: return (s(i%2)%d)*((s(i//2)**2)%d)
+print(sum([a[i]*((s(i)-1)-(s(n-1-i)-1)) for i in range(n)])%d)
