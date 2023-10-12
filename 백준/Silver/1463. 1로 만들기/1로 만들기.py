b6e21b0408338d80668a9197f26c99d696e8442d
terryp-ds <140671290+terryp-ds@users.noreply.github.com>
2023-10-12 12:51:18 +0900

    [Silver III] Title: 1로 만들기, Time: 648 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,24 +1,7 @@
-from collections import deque
-
-n = int(input())
-queue = deque([1])
-visited = [0]*(10**6+1)
-
-if n == 1:
-    print(0)
-    
-else:
-    while 1:
-        cur = queue.popleft()
-
-        if cur == n:
-            print(visited[cur]-1)
-            break
-        
-        if not visited[cur]:
-            visited[cur] = 1
-
-        for nex in [cur+1, cur*2, cur*3]:
-            if nex <= n and not visited[nex]:
-                queue.append(nex)
-                visited[nex] = visited[cur] + 1
+n=int(input())
+d=[0]*(n+1)
+for i in range(2,n+1):
+    d[i]=d[i-1]+1
+    for k in [2,3]:
+        if not i%k: d[i]=min(d[i],d[i//k]+1)
+print(d[n])
