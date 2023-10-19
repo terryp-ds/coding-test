ca2985a5054f9168317b7cdc7c4df54881bad23f
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-10-19 16:09:32 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,19 +1,11 @@
 import sys
-input = sys.stdin.readline
-
-n = int(input())
-arr = [int(input()) for _ in range(n)]
-
-if n < 3:
-    print(sum(arr))
-    
+input=sys.stdin.readline
+n=int(input())
+a=[int(input()) for _ in range(n)]
+d=[0]*n
+d[0]=a[0]
+if n==1: print(d[0])
 else:
-    dp = [0]*n
-
-    dp[0] = arr[0]
-    dp[1] = arr[0]+arr[1]
-
-    for i in range(2, n):
-        dp[i] = max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i])
-
-    print(dp[i])
+    d[1]=a[0]+a[1]
+    for i in range(2,n): d[i] = max(d[i-3]+a[i-1]+a[i], d[i-2]+a[i])
+    print(d[n-1])
