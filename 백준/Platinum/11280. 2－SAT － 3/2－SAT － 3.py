33a0504d2197c5db56053a9868316faf6fb6f57b
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-10-21 23:47:29 +0900

    [Platinum IV] Title: 2-SAT - 3, Time: 292 ms, Memory: 55956 KB -BaekjoonHub

--- 백준/Platinum/11280. 2－SAT － 3/2－SAT － 3.py
+++ 백준/Platinum/11280. 2－SAT － 3/2－SAT － 3.py
@@ -1,7 +1,6 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 sys.setrecursionlimit(10**6)
-
 def tarjan(V,E):
     g=[[] for _ in range(2*V+1)]
     for s,e in E:
@@ -9,7 +8,6 @@ def tarjan(V,E):
         g[-e]+=[s]
     q,s,c = [],[],[0]
     v,f=[[0 for _ in range(2*V+1)] for _ in range(2)]
-
     def dfs(n):
         c[0]+=1
         v[n]=c[0]
@@ -27,11 +25,9 @@ def tarjan(V,E):
                 if t==n: break
             s.append(p)
         return a
-
     for i in range(1,2*V+1):
         if not v[i]: dfs(i)
     return s
-
 V,E=map(int, input().split())
 E=[list(map(int, input().split())) for _ in range(E)]
 scc=tarjan(V,E)
@@ -43,4 +39,4 @@ for i in range(1,V+1):
     if d[i]==d[-i]:
         f=0
         break
-print([0,1][f])
+print(f)
