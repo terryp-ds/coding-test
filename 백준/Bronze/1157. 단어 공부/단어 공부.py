d5d4b47563872ba5f6298680284a2f75a6fdcf8b
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-10-31 21:57:13 +0900

    [Bronze I] Title: 단어 공부, Time: 112 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,8 +1,4 @@
 from collections import Counter
-s=input().upper()
-c=Counter(s)
-c=sorted(c.items(), key=lambda x: x[1])
-if len(c)==1 or c[-1][1] != c[-2][1]:
-    print(c[-1][0])
-else:
-    print('?')
+c=sorted(Counter(input().upper()).items(), key=lambda x: x[1])
+if len(c)==1 or c[-1][1]!=c[-2][1]: print(c[-1][0])
+else: print('?')
