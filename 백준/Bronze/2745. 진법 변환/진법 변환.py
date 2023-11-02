d3da8230d0450af1da5dea4202b1e7ca3ee9d0d1
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-02 14:38:05 +0900

    [Bronze II] Title: 진법 변환, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,3 +1,3 @@
-a,b=input().split()
-d=dict(zip([str(i) for i in range(10)]+list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), range(36)))
-print(sum([d[x]*int(b)**(len(a)-i-1) for i,x in enumerate(a)]))
+n,b=input().split()
+d=dict(zip(list('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'),range(36)))
+print(sum([d[x]*int(b)**(len(n)-i-1) for i,x in enumerate(n)]))
