e9c8f2e3b1dcdd27ac606745382832601a8c9b42
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-03 10:22:53 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,2 +1,11 @@
 def solution(n, arr1, arr2):
-    return [str(bin(i|j))[2:].zfill(n).replace('0',' ').replace('1','#') for i,j in zip(arr1, arr2)]
+    arr1 = [str(bin(i)[2:]).zfill(n) for i in arr1]
+    arr2 = [str(bin(i)[2:]).zfill(n) for i in arr2]
+    
+    arr = []
+    
+    for r1,r2 in zip(arr1,arr2):
+        row = [' ' if i==j=='0' else '#' for i,j in zip(r1,r2)]
+        arr.append(''.join(row))
+
+    return arr
