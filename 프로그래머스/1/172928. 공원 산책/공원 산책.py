ac6ef11cdcc3a2244a64102a41f7c46d5b64e941
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-05 22:05:02 +0900

    [level 1] Title: 공원 산책, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -7,13 +7,9 @@ def solution(park, routes):
     for route in routes:
         o,n=route.split()
         n=int(n)
-        i,f=1,1
-        while i<=n:
+        for i in range(1,n+1):
             ny,nx=y+d[o][0]*i,x+d[o][1]*i
             if not (0<=ny<r and 0<=nx<c) or park[ny][nx]=='X': 
-                f=0
                 break
-            else: i+=1
-        if f: 
-            y,x=ny,nx
+        else: y,x=ny,nx
     return [y,x]
