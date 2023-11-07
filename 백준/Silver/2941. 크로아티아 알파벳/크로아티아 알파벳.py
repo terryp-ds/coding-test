9f938264d644e151f759ee64eaf220c60280fe47
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-07 09:14:27 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 128 ms, Memory: 34828 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,7 +1,2 @@
-from re import findall
-x=input()
-d=['c=','c-','dz=','d-','lj','nj','s=','z=']
-a=[]
-for s in d:
-    a.extend(findall(s,x))
-print(len(x)-len(a))
+import re
+print(len(re.sub('c=|c-|dz=|d-|lj|nj|s=|z=',' ',input())))
