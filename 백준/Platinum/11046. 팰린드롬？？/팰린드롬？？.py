80a23061a22999ad69dadbe3dd19da9fd6cf82bb
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-07 16:14:44 +0900

    [Platinum V] Title: 팰린드롬??, Time: 992 ms, Memory: 247664 KB -BaekjoonHub

--- 백준/Platinum/11046. 팰린드롬？？/팰린드롬？？.py
+++ 백준/Platinum/11046. 팰린드롬？？/팰린드롬？？.py
@@ -1,20 +1,14 @@
 import sys
 input=sys.stdin.readline
-input()
-a=input().strip().split()
-b=[-1]*(len(a)*2+1)
-b[1::2] = a
-m=len(b)
-p=[0]*m
-r,c=-1,-1
-for i in range(m-1):
-    if r>=i:
-        p[i]=min(r-i, p[c*2-i])
-    while i+p[i]+1<m and b[i+p[i]+1] == b[i-p[i]-1]:
-        p[i]+=1
-    if i+p[i]>r:
-        r=i+p[i]
-        c=i
-for _ in range(int(input())):
-    e,f = map(int,input().split())
-    print(0+(f-e<=p[e+f-1]))
+t=[0]*(2*int(input())+1)
+t[1::2]=input().strip().split()
+n=len(t)
+a=[0]*n 
+r,p=0,0
+for i in range(n):
+    if i<=r: a[i]=min(a[2*p-i],r-i)
+    while i+a[i]+1<n and t[i-a[i]-1]==t[i+a[i]+1]: a[i]+=1
+    if r<i+a[i]: r=i+a[i]; p=i
+for _ in range(int(input())): 
+    s,e=map(int,input().split())
+    print((a[s+e-1]>=e-s)+0)
