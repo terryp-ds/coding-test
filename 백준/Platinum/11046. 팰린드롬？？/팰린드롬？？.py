734fd8a63ec1009c1ae9325497450a7d8d03d637
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-07 21:58:54 +0900

    [Platinum V] Title: 팰린드롬??, Time: 4016 ms, Memory: 130428 KB -BaekjoonHub

--- 백준/Platinum/11046. 팰린드롬？？/팰린드롬？？.py
+++ 백준/Platinum/11046. 팰린드롬？？/팰린드롬？？.py
@@ -6,9 +6,9 @@ n=len(t)
 a=[0]*n 
 r,p=0,0
 for i in range(n):
-    if i<=r: a[i]=min(a[2*p-i],r-i)
-    while i+a[i]+1<n and t[i-a[i]-1]==t[i+a[i]+1]: a[i]+=1
-    if r<i+a[i]: r=i+a[i]; p=i
+    if i<=r:a[i]=min(a[2*p-i],r-i)
+    while i+a[i]+1<n and t[i-a[i]-1]==t[i+a[i]+1]:a[i]+=1
+    if r<i+a[i]:r=i+a[i];p=i
 for _ in range(int(input())): 
     s,e=map(int,input().split())
     print((a[s+e-1]>=e-s)+0)
