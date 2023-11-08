7a1075c04f96c544e84a367da44072b173fcdfb9
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-08 21:19:15 +0900

    [Gold V] Title: 토마토, Time: 2004 ms, Memory: 146632 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,31 +1,18 @@
 import sys
-
-input = sys.stdin.readline
-
-m,n = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-stack = [[y,x] for x in range(m) for y in range(n) if graph[y][x] == 1]
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-day = 0
-
-while stack:
-    new_stack = []
-
-    for y,x in stack:
-
+input=sys.stdin.readline
+m,n=map(int,input().split())
+a=[[*map(int,input().split())] for _ in range(n)]
+q=[(y,x) for x in range(m) for y in range(n) if a[y][x]==1]
+dy,dx=[1,-1,0,0],[0,0,1,-1]
+d=0
+while q:
+    nq=[]
+    for y,x in q:
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-
-            if 0 <= nx < m and 0 <= ny < n:
-                if not graph[ny][nx]:
-                    new_stack.append([ny, nx])
-                    graph[ny][nx] = 1
-
-    stack = new_stack
-
-    if stack:
-        day += 1
-
-print(day if not sum(map(lambda x: x.count(0), graph)) else -1)
+            ny,nx=y+dy[i],x+dx[i]
+            if not (0<=ny<n and 0<=nx<m) or a[ny][nx]: continue
+            a[ny][nx]=1
+            nq.append((ny,nx))
+    if nq:d+=1
+    q=nq
+print([d,-1][bool(sum([r.count(0) for r in a]))])
