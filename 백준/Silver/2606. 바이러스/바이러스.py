5e6a736a6475ba45463742e55a6e72c4cb030d2c
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-08 15:39:46 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,18 @@
 import sys
 input=sys.stdin.readline
-
-n,m=int(input()),int(input())
+n=int(input())
 g=[[] for _ in range(n+1)]
-e=[list(map(int, input().split())) for _ in range(m)]
-
-for u,v in e:
-    g[u].append(v)
-    g[v].append(u)
-
 v=[0]*(n+1)
-s=[1]
-
-while s:
-    c=s.pop()
-    if not v[c]:
-        v[c]=1
-        for x in g[c]:
-            if not v[x]:
-                s.append(x)
-
+for _ in range(int(input())):
+    s,e=map(int,input().split())
+    g[s]+=[e]
+    g[e]+=[s]
+q=[1]
+v[1]=1
+while q:
+    c=q.pop()
+    for x in g[c]:
+        if v[x]: continue
+        v[x]=1
+        q+=[x]
 print(sum(v)-1)
