a3a5f750bc79dc8ba9693f35f709b99df380c411
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-08 15:05:59 +0900

    [Silver III] Title: 피보나치 함수, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,26 +1,3 @@
-import sys
-
-input = sys.stdin.readline
-
-n = int(input())
-
-dict_0 = {0:0, 1:1}
-dict_1 = {0:1, 1:0}
-
-def fib_0(k):
-    if k not in dict_0:
-        dict_0[k] = fib_0(k-1) + fib_0(k-2)
-
-    return dict_0[k]
-
-
-def fib_1(k):
-    if k not in dict_1:
-        dict_1[k] = fib_1(k-1) + fib_1(k-2)
-
-    return dict_1[k]
-
-
-for _ in range(n):
-    k = int(input())
-    print(f'{fib_1(k)} {fib_0(k)}')
+a=[[1,0],[0,1]]
+for i in range(2,41): a+=[[a[i-2][0]+a[i-2][1],a[i-1][0]+a[i-1][1]]]
+for _ in range(int(input())): print(*a[int(input())])
