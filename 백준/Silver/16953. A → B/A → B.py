e0b4b403a7d0f9164719e2823a33dfe794e7d63c
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-08 15:27:59 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,11 +1,8 @@
-a,b = map(int, input().split())
-c = 1
-while b > a:
-    if b%10 == 1:
-        b//=10
-    elif b%2:
-        break
-    else:
-        b//=2
+a,b=map(int,input().split())
+c=1
+while b>a:
+    if b%10==1: b//=10
+    elif b%2: break
+    else: b//=2
     c+=1
-print(c if a==b else -1)
+print([-1,c][a==b])
