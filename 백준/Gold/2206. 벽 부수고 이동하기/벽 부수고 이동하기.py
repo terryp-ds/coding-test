aa93c1fdb807e4e95db90198fc37a5dc35d38cd5
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-09 10:37:14 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 5976 ms, Memory: 180328 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,36 +1,22 @@
 from collections import deque
 import sys
-input = sys.stdin.readline
-
-n,m = map(int, input().split())
-arr = [list(map(int, list(input().rstrip()))) for _ in range(n)]
-visited = [[[0,0] for _ in range(m)] for _ in range(n)]
-
-dy = [1, -1, 0, 0]
-dx = [0, 0, 1, -1]
-
-queue = deque([(0,0,0)])
-visited[0][0][0] = 1
-
-while queue:
-    y,x,b = queue.popleft()
-
-    if y== n-1 and x == m-1:
-        print(visited[y][x][b])
-        break
-
+input=sys.stdin.readline
+n,m=map(int,input().split())
+a=[input().strip() for _ in range(n)]
+v=[[[0,0] for _ in range(m)] for _ in range(n)]
+dy,dx=[1,-1,0,0],[0,0,1,-1]
+q=deque([(0,0,0)])
+f=0
+while q:
+    y,x,b=q.popleft()
+    if (y,x)==(n-1,m-1): f=1; break
     for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-        
-        if 0 <= ny < n and 0 <= nx < m:
-            if not visited[ny][nx][b] and arr[ny][nx] == 0:
-                queue.append((ny, nx, b))
-                visited[ny][nx][b] = visited[y][x][b] + 1
-
-            if not b and arr[ny][nx] == 1 and not visited[ny][nx][1]:
-                queue.append((ny, nx, 1))
-                visited[ny][nx][1] = visited[y][x][b] + 1
-    
-if not (y == n-1 and x == m-1):
-    print(-1)
+        ny,nx=y+dy[i],x+dx[i]
+        if not (0<=ny<n and 0<=nx<m): continue
+        if a[ny][nx]=='0' and not v[ny][nx][b]:
+            v[ny][nx][b]=v[y][x][b]+1
+            q.append((ny,nx,b))
+        elif a[ny][nx]=='1' and not v[ny][nx][1] and b<1:
+            v[ny][nx][1]=v[y][x][f]+1
+            q.append((ny,nx,1))
+print(v[y][x][b]+1 if f else -1)
