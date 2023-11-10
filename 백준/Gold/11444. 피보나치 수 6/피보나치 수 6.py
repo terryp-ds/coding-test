499333441b8157db5958f5e40ecce78f30bfe9c4
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-10 21:37:56 +0900

    [Gold II] Title: 피보나치 수 6, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -1,25 +1,15 @@
-d = 1000000007
-
-def matmul(m1, m2):
-    new_m = []
-
-    for r in m1:
-        new_r = []
-
-        for i, _ in enumerate(r):
-            new_r.append(sum(map(lambda x,y:x*y, r, [r2[i] for r2 in m2])) % d)
-
-        new_m.append(new_r)
-        
-    return new_m
-
-def power(x, n):
-    if n == 1:
-        return x
-    elif n % 2:
-        return matmul(x, power(x, n-1))
+d=10**9+7
+def mul(x,y):
+    m=[]
+    for r in x:
+        a=[]
+        for i,_ in enumerate(r): a+=[sum(map(lambda x,y:x*y,r,[j[i] for j in y]))%d]
+        m.append(a)
+    return m
+def pow(x,n):
+    if n==1: return x
+    elif n%2: return mul(x,pow(x,n-1))
     else:
-        p = power(x,n//2)
-        return matmul(p,p)
-
-print(power([[1,1],[1,0]], int(input()))[0][1])
+        p=pow(x,n//2)
+        return mul(p,p)
+print(pow([[1,1],[1,0]],int(input()))[0][1])
