e1c7823a3a9c88c16e70fa9817fa7e688e516301
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-11 17:28:11 +0900

    [Silver III] Title: 파도반 수열, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,14 +1,5 @@
 import sys
-
-input = sys.stdin.readline
-n = int(input())
-arr = [1,1,1,2,2]
-c = 5
-for _ in range(n):
-    k = int(input())
-
-    while c < k:
-        arr.append(arr[-1]+arr[-5])
-        c += 1
-
-    print(arr[k-1])
+input=sys.stdin.readline
+p=[0,1,1]+[0]*98
+for i in range(3,101):p[i]=p[i-2]+p[i-3]
+for _ in range(int(input())):print(p[int(input())])
