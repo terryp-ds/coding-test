007ec6e96318187bdbca8c2c325b275e8197b8a7
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-12 21:41:19 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 384 ms, Memory: 112104 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,32 +1,27 @@
-import sys
-input=sys.stdin.readline
-r,c,t=map(int, input().split())
-arr=[[*map(int,input().split())] for _ in range(r)]
+r,c,t=map(int,input().split())
+a=[[*map(int,input().split())] for _ in range(r)]
 dy,dx=[1,-1,0,0],[0,0,1,-1]
 for y in range(r):
-    if arr[y][0]==-1:
-        loc=(y,0)
-        break
+    if a[y][0]==-1: l=(y,0); break
 for _ in range(t):
-    arr2=[[0]*c for _ in range(r)]
+    a2=[[0]*c for _ in range(r)]
     for y in range(r):
         for x in range(c):
-            if arr[y][x]>0:
-                cnt=0
-                for i in range(4):
-                    ny,nx= y+dy[i],x+dx[i]
-                    if 0<=ny<r and 0<=nx<c:
-                        if arr[ny][nx]!=-1:
-                            arr2[ny][nx]+=arr[y][x]//5
-                            cnt+=1
-                arr2[y][x]+=arr[y][x]-((arr[y][x]//5)*cnt)
-            elif arr[y][x]==-1: arr2[y][x] = -1
-    y,x = loc
-    for i in range(y-1,0,-1): arr2[i][0]=arr2[i-1][0]
-    for i in range(y+2, r-1): arr2[i][0]=arr2[i+1][0]
-    for i in [0,-1]: arr2[i]=arr2[i][1:]+[0]
-    for i in range(y): arr2[i][-1]=arr2[i+1][-1]
-    for i in range(r-1,y+1,-1): arr2[i][-1]=arr2[i-1][-1]
-    for i in range(y,y+2): arr2[i]=[-1,0]+arr2[i][1:-1]
-    arr = arr2
-print(sum(map(sum,arr))+2)
+            if a[y][x]<0: continue
+            if a[y][x]==-1: a2[y][x]=-1; continue
+            n=0
+            for i in range(4):
+                ny,nx=y+dy[i],x+dx[i]
+                if not (0<=ny<r and 0<=nx<c) or a[ny][nx]==-1: continue
+                a2[ny][nx]+=a[y][x]//5
+                n+=1
+            a2[y][x]+=a[y][x]-((a[y][x]//5)*n)
+    y,x=l
+    for i in range(y-1,0,-1): a2[i][0]=a2[i-1][0]
+    for i in range(y+2,r-1): a2[i][0]=a2[i+1][0]
+    for i in [0,-1]: a2[i]=a2[i][1:]+[0]
+    for i in range(y): a2[i][-1]=a2[i+1][-1]
+    for i in range(r-1,y+1,-1): a2[i][-1]=a2[i-1][-1]
+    for i in range(y,y+2): a2[i]=[-1,0]+a2[i][1:-1]
+    a=a2
+print(sum(map(sum,a))+2)
