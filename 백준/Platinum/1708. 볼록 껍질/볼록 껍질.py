479bde7b964d0bc0a14f6ad45751c217631eced7
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-14 16:18:55 +0900

    [Platinum V] Title: 볼록 껍질, Time: 4460 ms, Memory: 51508 KB -BaekjoonHub

--- 백준/Platinum/1708. 볼록 껍질/볼록 껍질.py
+++ 백준/Platinum/1708. 볼록 껍질/볼록 껍질.py
@@ -1,23 +1,13 @@
-def ccw(a,b,c):
-    return ((b[0]-a[0]) * (c[1]-a[1]) - (b[1]-a[1]) * (c[0]-a[0])) > 0
-
-def convex_hull(coords):
-    stack = []
-
-    for c in coords:
-
-        while len(stack) > 1:
-            a,b = stack[-2], stack[-1]
-
-            if ccw(a,b,c):
-                break
-
-            stack.pop()
-
-        stack.append(c)
-
-    return len(stack)
-
-n = int(input())
-coords = sorted([list(map(int, input().split())) for _ in range(n)])
-print(convex_hull(coords)+convex_hull(coords[::-1])-2)
+def ccw(a,b,c): return ((b[0]-a[0])*(c[1]-a[1])-(b[1]-a[1])*(c[0]-a[0]))>0
+def cv(a):
+    q=[]
+    for c in a:
+        while len(q)>1:
+            a,b=q[-2:]
+            if ccw(a,b,c):break
+            q.pop()
+        q+=[c]
+    return len(q)
+n=int(input())
+a=sorted([[*map(int,input().split())] for _ in range(n)])
+print(cv(a)+cv(a[::-1])-2)
