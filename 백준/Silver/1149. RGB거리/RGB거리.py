bec06e73bf203851c0d2b4b932d2bc8c52be203b
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-14 10:24:57 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,11 +1,8 @@
 import sys
-input = sys.stdin.readline
-
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-
+input=sys.stdin.readline
+n=int(input())
+a=[[*map(int,input().split())] for _ in range(n)]
 for i in range(1,n):
     for j in range(3):
-        arr[i][j]= min(arr[i-1][(j+1)%3],arr[i-1][(j+2)%3]) + arr[i][j]
-
-print(min(arr[n-1]))
+        a[i][j]=min(a[i-1][(j+1)%3],a[i-1][(j+2)%3])+a[i][j]
+print(min(a[n-1]))
