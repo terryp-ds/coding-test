287522fdce49915925d9da089841c2e5eb4f8a5c
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-14 10:58:27 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -5,16 +5,8 @@ def solution(nums):
     n = 0
     for c in comb:
         s = sum(c)
-        if not s%2: continue
+        if not s&1: continue
         for i in range(3,int(s**0.5)+1,2):
             if not s%i: break
         else: n+=1
     return n
-            
-            
-    answer = -1
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
-    return answer
