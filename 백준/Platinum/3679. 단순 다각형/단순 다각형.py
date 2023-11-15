49db04b9f5ab6ed8816ee9a9372e35b9c627b612
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-15 23:04:54 +0900

    [Platinum IV] Title: 단순 다각형, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/3679. 단순 다각형/단순 다각형.py
+++ 백준/Platinum/3679. 단순 다각형/단순 다각형.py
@@ -1,18 +1,15 @@
-def ccw(a,b,c): return ((b[0]-a[0])*(c[1]-a[1])-(b[1]-a[1])*(c[0]-a[0]))>=0
-def convex_hull(p):
-    s=[]
-    for c in p:
-        while len(s)>1:
-            a,b=s[-2:]
-            if ccw(a,b,c): break
-            s.pop()
-        s.append(c)
-    return s
 for _ in range(int(input())):
     n,*a=[*map(int,input().split())]
     a=sorted(zip(a[::2],a[1::2],range(n)))
-    c=[i[2] for i in convex_hull(a)]
-    s=set(c)
+    q=[]
+    for z in a:
+        while len(q)>1:
+            x,y=q[-2:]
+            if (y[0]-x[0])*(z[1]-x[1])-(y[1]-x[1])*(z[0]-x[0])>=0: break
+            q.pop()
+        q+=[z]
+    q=[i[2] for i in q]
+    s=set(q)
     for i in range(n-1,-1,-1):
-        if a[i][2] not in s: c+=[a[i][2]]
-    print(*c)
+        if a[i][2] not in s: q+=[a[i][2]]
+    print(*q)
