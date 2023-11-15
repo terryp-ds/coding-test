544f3bc0309d4e1f81b7833bc6ede409aa437f9e
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-15 22:46:58 +0900

    [Silver V] Title: Base Conversion, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11576. Base Conversion/Base Conversion.py
+++ 백준/Silver/11576. Base Conversion/Base Conversion.py
@@ -1,11 +1,8 @@
 a,b=map(int,input().split())
 m=int(input())
 s=sum([x*(a**(m-i-1)) for i,x in enumerate([*map(int,input().split())])])
-n=0
-while b**n<s: n+=1
 a=[]
-for i in range(n):
-    q=b**(n-1-i)
-    p,s=s//q,s%q
-    a+=[p]
-print(*a)
+while s:
+    a+=[s%b]
+    s//=b
+print(*a[::-1])
