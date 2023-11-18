44203aa422978944e40a4d9613202ca35f3fd2d1
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-18 20:59:40 +0900

    [Silver I] Title: 정수 삼각형, Time: 116 ms, Memory: 40732 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,26 +1,9 @@
 import sys
-
-input = sys.stdin.readline
-
-arr = [list(map(int, input().split())) for _ in range(int(input()))]
-
-ans = [0]
-ans2 = []
-
-for row in arr:
-    
-    for idx, num in enumerate(row):
-        
-        if idx == 0:
-            ans2.append(ans[idx]+num)
-            
-        elif idx == len(row)-1:
-            ans2.append(ans[-1]+num)
-            
-        else:
-            ans2.append(max(ans[idx-1], ans[idx])+num)
-            
-    ans = ans2
-    ans2 = []
-        
-print(max(ans))
+input=sys.stdin.readline
+n=int(input())
+a=[[*map(int,input().split())] for _ in range(n)]
+d=[[] for _ in range(n)]
+d[0]=a[0]
+if n>1:d[1]=[a[0][0]+a[1][0],a[0][0]+a[1][1]]
+for i in range(2,n): d[i]=[d[i-1][0]+a[i][0]]+[max(d[i-1][j-1],d[i-1][j])+a[i][j] for j in range(1,i)]+[d[i-1][-1]+a[i][-1]]
+print(max(d[n-1]))
