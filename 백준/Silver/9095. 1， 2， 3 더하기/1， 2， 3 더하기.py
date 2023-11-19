41c2b6a2cd56a24a620d6cb1bb220382aa4e0385
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-19 19:29:02 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,15 +1,3 @@
-import sys
-input = sys.stdin.readline
-
-for _ in range(int(input())):
-    n = int(input())
-    a,b,c = 1,2,4
-
-    if n < 3:
-        print([1,2,4][n-1])
-
-    else:
-        for _ in range(n-3):
-            a,b,c = b,c,a+b+c
-
-        print(c)
+d=[1,2,4]
+for i in range(3,10):d+=[sum(d[i-3:i])]
+for _ in range(int(input())):print(d[int(input())-1])
