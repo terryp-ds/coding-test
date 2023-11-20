b3dd14de19f85378a9767c2909cfa6ba74352d79
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-20 09:35:24 +0900

    [Silver IV] Title: 로프, Time: 116 ms, Memory: 36148 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,9 +1,8 @@
 import sys
 input=sys.stdin.readline
 a=sorted([int(input()) for _ in range(int(input()))], reverse=True)
-s,n,m=0,0,0
+n,m=0,0
 for i in a:
-    s+=i
     n+=1
     m=max(m,i*n)
 print(m)
