25622a5bcbba5e833db166dfd25dce47ff3f28b8
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-21 09:50:19 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 48 ms, Memory: 34432 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
@@ -1,7 +1,6 @@
-n=int(input())
-a=[*map(int,input().split())]
+input()
 m,s,c=0,0,0
-for i in a:
-    if i>c: m,s,c=max(m,s),0,i
-    else: s+=1
+for i in [*map(int,input().split())]:
+ if i>c:m,s,c=max(m,s),0,i
+ else:s+=1
 print(max(m,s))
