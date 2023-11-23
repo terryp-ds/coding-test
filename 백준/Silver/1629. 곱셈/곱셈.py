4c6644725f7df45837463309de394182000a1743
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-23 20:49:27 +0900

    [Silver I] Title: 곱셈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,13 +1,1 @@
-from functools import reduce
-
-a,b,c = map(int, input().split())
-
-b2 = bin(b)[2:][::-1]
-
-d = {0:a%c}
-
-for i in range(1, len(b2)):
-    d[i] = d[i-1]**2 % c
-
-print(reduce(lambda x,y:x*y%c, [d[i] for i,j in enumerate(b2) if int(j)]) % c)
-
+print(pow(*map(int,input().split())))
