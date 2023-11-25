323df0487501dadb46f71eb4e16612a8fa76d7f1
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-25 22:08:26 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,9 +1,3 @@
 from itertools import combinations
-import sys
-input = sys.stdin.readline
-
-n,m = map(int, input().split())
-
-comb = [' '.join([str(i) for i in p]) for p in combinations(range(1, n+1), m)]
-
-print(*comb, sep='\n')
+n,m=map(int,input().split())
+for c in combinations(range(1,n+1),m): print(*c)
