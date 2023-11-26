87d9f0cc263cdd35bc988e8fa1a50b7760b0f4e0
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-26 18:46:02 +0900

    [Silver III] Title: 게임, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,10 +1,5 @@
 x,y=map(int,input().split())
 z=y*100//x
-if z>=99:print(-1)
-else:
-    a,s,e=0,1,x
-    while s<=e:
-        m=(s+e)//2
-        if (y+m)*100//(x+m)<=z: s=m+1
-        else: a,e=m,m-1
-    print(a)
+n=z*x+x-y*100
+d=99-z
+print(-1 if z>98 else n//d+(n%d>0))
