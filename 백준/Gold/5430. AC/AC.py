64025f9a0bac7a3e52f8310fc0b113454205a1b8
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-27 23:29:45 +0900

    [Gold V] Title: AC, Time: 1276 ms, Memory: 108584 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,35 +1,12 @@
 from collections import deque
 import sys
-
-n_case=int(input())
-
-for i in range(n_case):
-    func, len_arr, arr = [sys.stdin.readline().rstrip() for j in range(3)]
-    func, len_arr, arr = deque(func), int(len_arr), deque(arr[1:-1].split(','))
-    
-    reverse = 0
-    flag = 1
-    
-    if len_arr == 0:
-        arr = []
-        
-    for letter in func:
-        if letter == 'R':
-            reverse = 1 - reverse
-            
-        else:
-            if len(arr) < 1:
-                flag = 0
-                print("error")
-                break
-                
-            else:
-                if reverse:
-                    arr.pop()
-                else:
-                    arr.popleft()
-                    
-    if flag:
-        if reverse:
-            arr = list(arr)[::-1]
-        print('['+','.join(arr)+']')
+input=sys.stdin.readline
+for _ in range(int(input())):
+    c,_,a=input().strip(),int(input()),deque(eval(input().strip()))
+    f=r=1
+    for i in c:
+        if i=='R':r=-r;continue
+        if not a:f=0;print('error');break
+        if r<0:a.pop()
+        else:a.popleft()
+    if f:print(f'[{",".join(map(str,list(a)[::r]))}]')
