9ba8532523ef0a1ab76e25318309ac519c022d44
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-11-29 15:06:20 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
@@ -1,12 +1,3 @@
-from collections import deque
-a,k=map(int,input().split())
-q=deque([a])
-v={a:0}
-while q:
-    c=q.popleft()
-    if c==k: break
-    for x in [2*c,c+1]:
-        if x in v or x>k: continue
-        v[x]=v[c]+1
-        q+=[x]
-print(v[k])
+a,k=map(int,input().split());c=0
+while a<=k//2:c+=1+(k&1);k//=2
+print(c+k-a)
