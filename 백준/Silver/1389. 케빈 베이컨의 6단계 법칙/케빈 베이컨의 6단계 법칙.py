f0e49fa94d53ea1c12e079431cc8691e6dda0786
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-01 00:12:23 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 68 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,11 +1,34 @@
-n,m=map(int,input().split())
-a=[[*map(int,input().split())] for _ in range(m)]
-v=[[101 for _ in range(n+1)] for _ in range(n+1)]
-for i in range(1,n+1): v[i][i]=0
-for s,e in a: v[s][e]=v[e][s]=1
-for k in range(1,n+1):
-   for i in range(1,n+1):
-       for j in range(1,n+1):
-            v[i][j]=min(v[i][j],v[i][k]+v[k][j])
-v=[sum(r) for r in v]
-print(v.index(min(v)))
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+score = [[0]*n for _ in range(n)]
+edges = [list(map(int, input().split())) for _ in range(m)]
+graph = [[] for _ in range(n)]
+
+for v1,v2 in edges:
+    graph[v1-1].append(v2-1)
+    graph[v2-1].append(v1-1)
+
+for i in range(n):
+    visited = [0]*n
+    queue = deque([i])
+    c = 0
+
+    while queue:
+        cur = queue.popleft()
+
+        if not visited[cur]:
+            visited[cur] = 1
+            c = score[i][cur]
+
+            for nex in graph[cur]:
+                if not visited[nex]:
+                    queue.append(nex)
+
+                    if not score[i][nex]:
+                        score[i][nex] = c+1
+
+print(sorted(zip(range(1,n+1), map(sum, score)), key=lambda x:(x[1], x[0]))[0][0])
