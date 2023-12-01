7a4fe78a8a8b4fb25ae46dec8e48d2713bbf11d8
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-01 23:33:49 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,25 +1,3 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
-
-n,m = map(int, input().split())
-
-cnt = 1
-queue = deque([[i] for i in range(1,n+1)])
-
-while cnt < m:
-
-    queue2 = deque()
-
-    while queue:
-        node = queue.popleft()
-        
-        for i in range(node[-1], n+1):
-            queue2.append(node+[i])
-
-    queue = queue2
-    cnt += 1
-
-
-for arr in queue:
-    print(*arr, sep=' ')
+from itertools import *
+n,m=map(int,input().split())
+for c in combinations_with_replacement(range(1,n+1),m): print(*c)
