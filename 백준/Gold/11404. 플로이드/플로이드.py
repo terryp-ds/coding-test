531142e5e65ae753090fac9c4b665d1aec6fdd75
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-02 19:08:31 +0900

    [Gold IV] Title: 플로이드, Time: 708 ms, Memory: 48284 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,27 +1,13 @@
 import sys
-input = sys.stdin.readline
-
-n = int(input())
-m = int(input())
-arr = [list(map(int, input().split())) for _ in range(m)]
-dist = [[10**8+1 for _ in range(n+1)] for _ in range(n+1)]
-
-for i in range(1,n+1):
-    dist[i][i] = 0
-
-for v1, v2, w in arr:
-    dist[v1][v2] = min(w, dist[v1][v2])
-
+input=sys.stdin.readline
+n,m=int(input()),int(input())
+a=[[*map(int,input().split())] for _ in range(m)]
+v=[[10**8 for _ in range(n+1)] for _ in range(n+1)]
+for i in range(1,n+1): v[i][i]=0
+for s,e,w in a: v[s][e]=min(w,v[s][e])
 for k in range(1,n+1):
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-                
-for i in range(1,n+1):
-    row = dist[i][1:]
-
-    for j in range(n):
-        if row[j] == 10**8+1:
-            row[j] = 0
-
-    print(*row, sep=' ')
+   for i in range(1,n+1):
+       for j in range(1,n+1):
+            v[i][j]=min(v[i][j],v[i][k]+v[k][j])
+for i in range(1,n+1): v[i][i]=0
+for r in v[1:]: print(*[[0,i][i<10**8] for i in r[1:]])
