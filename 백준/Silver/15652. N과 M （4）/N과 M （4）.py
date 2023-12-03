9ada0b14bb0d4946d35162db941fbfff1ba8b238
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-03 23:53:47 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,3 +1,3 @@
 from itertools import *
 n,m=map(int,input().split())
-for c in combinations_with_replacement(range(1,n+1),m): print(*c)
+for c in combinations_with_replacement(range(1,n+1),m):print(*c)
