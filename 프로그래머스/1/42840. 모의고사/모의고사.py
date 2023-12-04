1eb0fb8f781517162627a1fcf6f8d22b7ff15b56
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-04 23:09:33 +0900

    [level 1] Title: 모의고사, Time: 4.42 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,5 +1,4 @@
 def solution(answers):
-    n=len(answers)
     pat = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
-    answer = [sum([p[i%len(p)]==answers[i] for i in range(n)]) for p in pat]
+    answer = [sum([p[i%len(p)]==answers[i] for i in range(len(answers))]) for p in pat]
     return [i+1 for i in range(3) if answer[i]==max(answer)]
