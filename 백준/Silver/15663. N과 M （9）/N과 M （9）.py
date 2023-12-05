91bb61bc2324eb7767d20cc1b62e2583c91c0634
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-05 21:27:00 +0900

    [Silver II] Title: N과 M (9), Time: 104 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,26 +1,3 @@
-n,m = map(int, input().split())
-arr = sorted(list(map(int, input().split())))
-stack = []
-prev = 0
-visited = [0]*n
-
-def dfs():
-    prev = 0
-    
-    if len(stack) == m and prev != stack:
-        print(*stack)
-        return None
-
-    else:
-        for i in range(n):
-            num = arr[i]
-            
-            if num != prev and not visited[i]:
-                stack.append(num)
-                prev = num
-                visited[i] = 1
-                dfs()
-                stack.pop()
-                visited[i] = 0
-
-dfs()
+from itertools import *
+n,m=map(int,input().split())
+for p in sorted(set(permutations(map(int,input().split()),m))):print(*p)
