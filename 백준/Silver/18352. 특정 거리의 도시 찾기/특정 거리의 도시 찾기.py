6f81598da2569303e7e9ab822fdc71128e848a73
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-05 21:45:11 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2044 ms, Memory: 101820 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,19 @@
+from collections import deque
+import sys
+input=sys.stdin.readline
+n,m,k,x=map(int,input().split())
+g=[[] for _ in range(n+1)]
+for _ in range(m):
+    s,e=map(int,input().split())
+    g[s]+=[e]
+v=[10**11]*(n+1)
+q=deque([x])
+v[x]=0
+while q:
+    c=q.popleft()
+    for x in g[c]:
+        if v[c]+1<v[x]: 
+            v[x]=v[c]+1
+            q+=[x]
+a=[i for i in range(n+1) if v[i]==k]
+print(*[[-1],a][bool(a)],sep='\n')
