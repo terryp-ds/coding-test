75d3b22a5261d335e4c1d65f378c2317504b22dc
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-06 23:43:43 +0900

    [Silver II] Title: N과 M (12), Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,15 +1,3 @@
-n,m = map(int, input().split())
-arr = sorted(set(map(int, input().split())), reverse=True)
-
-stack = [[i] for i in arr]
-
-while stack:
-    num = stack.pop()
-    
-    if len(num) == m:
-        print(*num)
-        continue
-    
-    for i in arr:
-        if i >= num[-1]:
-            stack.append(num+[i])
+from itertools import *
+n,m=map(int,input().split())
+for c in combinations_with_replacement(sorted(set(map(int,input().split()))),m):print(*c)
