d59bb09f2687630a88a1b0d35c95010a905b3427
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-14 20:55:20 +0900

    [Silver IV] Title: 듣보잡, Time: 84 ms, Memory: 44228 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,6 +1,6 @@
 import sys
-input=sys.stdin.readline
+input=lambda:sys.stdin.readline().strip()
 m,n=map(int,input().split())
-s=set([input().strip() for _ in range(m)])&set([input().strip() for _ in range(n)])
+s=set([input() for _ in range(m)])&set([input() for _ in range(n)])
 print(len(s))
 print(*sorted(s),sep='\n')
