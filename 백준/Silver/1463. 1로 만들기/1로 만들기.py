23aa31d3b5c12a60fcb7c5d439890c58742b707c
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-16 23:22:30 +0900

    [Silver III] Title: 1로 만들기, Time: 660 ms, Memory: 39064 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,7 +1,7 @@
 n=int(input())
 d=[0]*(n+1)
 for i in range(2,n+1):
-    d[i]=d[i-1]+1
-    for k in [2,3]:
-        if not i%k: d[i]=min(d[i],d[i//k]+1)
+ d[i]=d[i-1]+1
+ for k in (2,3):
+  if i%k==0:d[i]=min(d[i],d[i//k]+1)
 print(d[n])
