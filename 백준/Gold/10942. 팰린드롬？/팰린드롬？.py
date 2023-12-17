85aab350a9a0c4e0e85a2ac70117c7d715ad3794
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-17 23:58:08 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2072 ms, Memory: 61960 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -1,13 +1,11 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 n=int(input())
 a=[*map(int,input().split())]
-dp=[[0]*n for _ in range(n)]
-
+d=[[0]*n for _ in range(n)]
 for i in range(n-1,-1,-1):
-    for j in range(i,n):
-        if a[i]==a[j] and (i+2>j or dp[i+1][j-1]): dp[i][j]=1
-
+ for j in range(i,n):
+  if a[i]==a[j] and (i+2>j or d[i+1][j-1]):d[i][j]=1
 for _ in range(int(input())):
-    s,e=map(int,input().split())
-    print(dp[s-1][e-1])
+ s,e=map(int,input().split())
+ print(d[s-1][e-1])
