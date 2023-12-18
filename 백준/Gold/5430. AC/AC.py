d821658af114eac45bde16dacac28860f6a69f32
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-18 23:18:08 +0900

    [Gold V] Title: AC, Time: 1276 ms, Memory: 108584 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -2,11 +2,11 @@ from collections import deque
 import sys
 input=sys.stdin.readline
 for _ in range(int(input())):
-    c,_,a=input().strip(),int(input()),deque(eval(input().strip()))
-    f=r=1
-    for i in c:
-        if i=='R':r=-r;continue
-        if not a:f=0;print('error');break
-        if r<0:a.pop()
-        else:a.popleft()
-    if f:print(f'[{",".join(map(str,list(a)[::r]))}]')
+ c,_,a=input().strip(),int(input()),deque(eval(input().strip()))
+ f=r=1
+ for i in c:
+  if i=='R':r=-r;continue
+  if not a:f=0;print('error');break
+  if r<0:a.pop()
+  else:a.popleft()
+ if f:print(f'[{",".join(map(str,list(a)[::r]))}]')
