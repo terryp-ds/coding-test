5481a01d632deb71787f1d1f7c7d1b77b82a1e0b
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2023-12-22 23:05:02 +0900

    [Gold IV] Title: RGB거리 2, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.py
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.py
@@ -1,19 +1,12 @@
 import sys
-input = sys.stdin.readline
-
-n = int(input())
-a = [list(map(int, input().split())) for _ in range(n)]
-ans = 10**7
-
+input=sys.stdin.readline
+n=int(input())
+a=[[*map(int,input().split())] for _ in range(n)]
+m=10**7
 for i in range(3):
-    dp = [[10**7]*3 for _ in range(n)]
-    dp[0][i] = a[0][i]
-
-    for j in range(1,n):
-        for k in range(3):
-            dp[j][k] = a[j][k] + min(dp[j-1][(k+1)%3], dp[j-1][(k+2)%3])
-
-    for j in range(1,3):
-        ans = min(ans, dp[-1][(i+j)%3])
-
-print(ans)
+ d=[[m]*3 for _ in range(n)]
+ d[0][i]=a[0][i]
+ for j in range(1,n):
+  for k in range(3):d[j][k]=a[j][k]+min(d[j-1][(k+1)%3],d[j-1][(k+2)%3])
+  for j in range(1,3):m=min(m,d[-1][(i+j)%3])
+print(m)
