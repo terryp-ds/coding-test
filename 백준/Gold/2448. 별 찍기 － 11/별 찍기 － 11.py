71369896f17ea0d3bac6875b837f3a86ea9c5b3e
박태환 (Taewhan Park) <140671290+terryp-ds@users.noreply.github.com>
2024-01-02 23:11:16 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 1608 ms, Memory: 322156 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -1,13 +1,13 @@
 n=int(input())
 a=[[' ']*(2*n-1) for _ in range(n)]
 def star(s,c,e):
-    if e-s==2:
-        a[s][c]='*'
-        a[s+1][c-1]='*'
-        a[s+1][c+1]='*'
-        a[s+2][c-2:c+3]=['*']*5
-    else:
-        m,d=(s+e)//2,(e-s)//2
-        for t in [(s,c,m),(m+1,c-d-1,e),(m+1,c+d+1,e)]: star(*t)
+ if e-s==2:
+  a[s][c]='*'
+  a[s+1][c-1]='*'
+  a[s+1][c+1]='*'
+  a[s+2][c-2:c+3]=['*']*5
+ else:
+  m,d=(s+e)//2,(e-s)//2
+  for t in [(s,c,m),(m+1,c-d-1,e),(m+1,c+d+1,e)]:star(*t)
 star(0,n-1,n-1)
 for i in a: print(*i,sep='')
